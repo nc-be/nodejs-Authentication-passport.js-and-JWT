f6e91af2e08aa844f12a9bc932f8f1f77e4afe77
Nelson <nc96business@outlook.com>
2024-06-25 22:17:56 -0500

    Impelementacion de hashing de passwords: servicios user y customer - Comentarios sobre logica de programacion con getConnection, ORM, BOOM, JOI retirados de los archivos user.service.js y customer.service.js

--- api/services/customer.service.js
+++ api/services/customer.service.js
@@ -3,13 +3,17 @@ const boom = require('@hapi/boom');
 
 const { models } = require('./../libs/sequelize');
 
+const bcrypt = require('bcrypt'); // NECESARIO PARA HACER HASHING DE CONTRASEÑAS
+
 class CustomerService {
 
   // INVESTIGAR - PORQUE ESTA VACIO?
   constructor(){}
 
   async create(data) {
-    const newCustomer = await models.Customer.create(data, {include:['user']});
+    // Datos antes de implementar hashing
+    /*
+    const newCustomer = await models.Customer.create(data, {include:['user']}); */
     // Dentro del servicio se agrega la expresion {include:['user']}
     /*
     'user' HACE REFERENCIA A LA ASOCIACION BelongsTo ENTRE EL MODELO 'customer.model.js' Y EL MODELO 'user.model.js' Donde 'user' ES EL ALIAS QUE SE LE DA A LA RELACION
@@ -19,6 +23,18 @@ class CustomerService {
 
     INCLUDE SE ENCARGA DE RESOLVER LAS ASOCIACIONES (asociacion1, asociacion2,...) Y ANIDAR LOS RESULTADOS A LA RESPUESTA FINAL DEL SERVIDOR (rta)
     */
+
+    // Datos despues de implementar hashing
+    const hashPass = await bcrypt.hash(data.user.password, 10); // Implementar hashing en el password (la informacion no se extrae directamente de 'data' sino de un subpad llamado 'user')
+    const newData = {
+      ...data,
+      user:{
+        ...data.user,
+        password: hashPass
+      }
+    } // clona la informacion de 'data' y 'data.user', pero reemplaza el valor del password del user con el valor generado por bcrypt.hash
+    const newCustomer = await models.Customer.create(newData, {include:['user']}); // crea cliente y usuario con el password hashed
+    delete newCustomer.dataValues.user.dataValues.password; // Borra el valor de password antes de retornar la informacion (Importante incluir dataValues - la informacion no se extrae directamente de 'data' sino de un subpad llamado 'user')
     return newCustomer;
   }
 
--- api/services/user.service.js
+++ api/services/user.service.js
@@ -1,12 +1,10 @@
-// - FALTA ANALIZAR
 const boom = require('@hapi/boom');
 
-// const getConnection = require('../libs/postgres'); // LLAMAR LA LIBRERIA 'postgres'
-// *******getConnection es reemplazado por pool**********
-
 const pool = require('../libs/postgres.pool');  // LLAMAR LA LIBRERIA 'postgres.pools' *******pool es reemplazado por sequelize (NO EN TODO)**********
-
 const { models } = require('./../libs/sequelize'); // LLAMAR LOS MODELOS CREADOS AL HACER init DE LA FUNCION setupModels UTILIZANDO sequelize
+
+const bcrypt = require('bcrypt'); // NECESARIO PARA HACER HASHING DE CONTRASEÑAS
+const { DatabaseModule } = require('@faker-js/faker');
 
 class UserService {
   constructor() {
@@ -15,35 +13,22 @@ class UserService {
   }
 
   async create(data) {
-    /*
-    // CREAR USUARIO - ANTES DE IMPLEMENTAR ORM
-    return data;
-    */
-    // CREAR USUARIO - DESPUES DE IMPLEMENTAR ORM
-    const newUser = await models.User.create(data); // El comando 'create' creara un usuario implementando la informacion recibida del esquema (data: id, email, password, role)
+    // Datos antes de implementar hashing
+    /* const newUser = await models.User.create(data); // El comando 'create' creara un usuario implementando la informacion recibida del esquema (data: id, email, password, role) */
+
+    // Datos despues de implementar hashing
+    const hashPass = await bcrypt.hash(data.password, 10); // Implementar hashing en el password
+    const newUser = await models.User.create(
+      {
+        ...data,
+        password: hashPass // reemplaza el valor del password del users con el valor generado por bcrypt.hash
+      }
+    ); // El comando 'create' creara un usuario implementando la informacion recibida del esquema (data: id, email, password, role)
+    delete newUser.dataValues.password; // Borra el valor de password antes de retornar la informacion (Importante incluir dataValues)
     return newUser; // retornar usuario
   }
 
   async find() {
-    // servicio 'find' con getConnection
-    /*
-    const client = await getConnection();   // obtener el cliente para la conexion
-    const rta = await client.query('SELECT * FROM tasks'); // Respuesta a la solicitud del servicio
-
-    //  SELECT * FROM tasks - Selecciona todo el contenido de la tabla 'tasks'
-
-    return await rta.rows; // Retorna las columnas de la tabla 'tasks'
-    */
-
-
-    // servicio 'find' con pool
-    /*
-    const query = 'SELECT * FROM tasks'; // GENERAR QUERY PARA CUMPLIR SOLICITUD DEL SERVICIO
-    const rta = await this.pool.query(query); // EJECUTA QUERY DE FORMA ASINCRONA
-    return rta.rows; // RETORNA LA LISTA DE PRODUCTOS
-    */
-
-
     // servicio 'find' con sequelize
     /*
     SINTAXIS
@@ -71,25 +56,14 @@ class UserService {
   }
 
   async findOne(id) {
-    // UTILIZANDO ORM
     const user = await models.User.findByPk(id); // 'findByPk' buscara el usuario que coincida con el 'id' obtenido
     if (!user) {
-      // CONDICIONAL QUE REVISA SI EL USUARIO EXISTE O NO - El condicional utiliza el middleware creado con boom  ./../middlewares/error.handler
-      throw boom.notFound('User not found'); // Lanza (throw) el error para que sea atrapado (catch) y validado por el esquema de validacion de data entrante (validatorHandler - ./../routes/users.router -- ./../middlewares/validator.handler) TLDR El esquema se encarga de validar el error y enviar la respuesta 'User not found' en el caso de que sea un error
+      throw boom.notFound('User not found');
     }
     return user;
   }
 
   async update(id, changes) {
-    /*
-    // UTILIZANDO JOI
-    return {
-      id,
-      changes,
-    };
-    */
-
-    // UTILIZANDO ORM
     // const user = await models.User.findByPk(); // 'findByPk' buscara el usuario que coincida con el 'id' obtenido
     const user = await this.findOne(id); // Mejora de la linea de codigo anterior - lleva a cabo el comando 'findByPk' y aplica el condicional que detecta si existe o no un usuario que coincida con el 'id' - envia error 'User not found' en caso contrario
     const rta = await user.update(changes); // Una vez se cuenta con el usuario (user) se aplican los cambios (changes) realizados sobre el utilizando el comando 'update'.
@@ -97,13 +71,6 @@ class UserService {
   }
 
   async delete(id) {
-    /*
-    // UTILIZANDO JOI
-    return { id };
-    */
-
-    // UTILIZANDO ORM
-    // const user = await models.User.findByPk(); // 'findByPk' buscara el usuario que coincida con el 'id' obtenido
     const user = await this.findOne(id); // Mejora de la linea de codigo anterior - lleva a cabo el comando 'findByPk' y aplica el condicional que detecta si existe o no un usuario que coincida con el 'id' - envia error 'User not found' en caso contrario
     await user.destroy(); // Una vez se cuenta con el usuario (user) se elimina de la lista de usuarios utilizando el comando 'update'.
     return { id }; // Enviar el 'id' del usuario eliminado como respuesta
